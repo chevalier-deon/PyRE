ee6b5573974152a6a408c39e1b600df2e351755c
chevalier-deon <molly.astral@gmail.com>
2021-10-21 00:46:48 -0400

    I made it resemble an actual library
    I altered the file structure so someone might actually use it as a library someday

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name = "pyre",
+    version = "0.0.1",
+    description = "A low-dependency library for epidemiologic analysis.",
+    url = "https://github.com/chevalier-deon/PyRE.git",
+    author = "chevalier-deon/@gurovain",
+    author_email = "do.not.email.rightnow@gmail.com",
+    license="MIT",
+    packages=["pyre"],
+    install_requires=[
+        "numpy",
+        "pandas"
+    ],
+    zip_safe=False)
