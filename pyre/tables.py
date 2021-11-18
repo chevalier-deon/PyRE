6d07fafb1223634bd4727bf423b188844aacf033
chevalier-deon <molly.astral@gmail.com>
2021-11-18 14:27:42 -0500

    odds.py

---
+++ pyre/tables.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from rates import mean, rate
+
+def crosstab():
+    return "i need help, immediately!"
+
+def repro():
+    return "please do something"
+
+def validity():
+    return "fuck sleep and his cousin yea"
